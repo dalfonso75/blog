1b3e7f8e1127620af0f613ba71859af78d7a5412
dalfonso75 <ea888416@gmail.com>
2022-07-16 10:01:27 -0500

    :art: Component Post

---
+++ src/components/Post/index.js
@@ -0,0 +1,38 @@
+import Author from "../Author";
+
+import styles from "./styles.module.css";
+
+export default function Post({
+  postId,
+  postImage,
+  postTitle,
+  postAuthor,
+  postTags,
+}) {
+  return (
+    <article className={styles["c-post"]}>
+      <header>
+        <img
+          className={styles["post__image"]}
+          src={postImage}
+          alt={postTitle}
+        />
+        <ul className={styles["post__tags"]}>
+          {postTags?.map((tag) => (
+            <li key={"key-" + tag} className={styles["post__tag"]}>
+              -{tag}
+            </li>
+          ))}
+        </ul>
+      </header>
+      <footer className={styles["post__content"]}>
+        <h3 className={styles["post__title"]}>{postTitle}</h3>
+        <Author
+          authorIMage={postAuthor.picture}
+          authorName={postAuthor.firstName + " " + postAuthor.lastName}
+        />
+        <button className={styles["post__read-more"]}>Leer más</button>
+      </footer>
+    </article>
+  );
+}
