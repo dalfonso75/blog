a0bf85437fa744538c5d8c08942d6ed2d245de7f
dalfonso75 <ea888416@gmail.com>
2022-07-16 11:23:46 -0500

    :construction: Archive

--- src/components/ArchivePost/index.js
+++ src/components/ArchivePost/index.js
@@ -3,20 +3,34 @@ import { useState, useEffect } from "react";
 import UseAllPosts from "../../hooks/useAllPosts";
 
 import Posts from "../Posts";
+import Tags from "../Tags";
+import Pagination from "../Pagination";
 
 import styles from "./styles.module.css";
-
 export default function ArchivePosts() {
-  const [posts, setPosts] = useState([])
   const { get, dataGet } = UseAllPosts();
+  const [posts, setPosts] = useState([]);
+
+  const [numberCurrentPage, setNumberCurrentPage] = useState(0);
+
   useEffect(() => {
-    get();
-    setPosts(dataGet);
-  }, [get]);
+    get(numberCurrentPage);
+  }, [get, numberCurrentPage]);
+
+  useEffect(() => {
+    if (dataGet) setPosts(dataGet);
+  }, [dataGet]);
+
   return (
     <div className={styles["c-archive-posts"]}>
-      {/*Componente tags */}
-      <Posts posts={posts}/>
+      <Tags setPosts={setPosts}/>
+      <Posts posts={posts} />
+      <Pagination
+        total={posts.total}
+        numberCurrentPage={numberCurrentPage}
+        setNumberCurrentPage={setNumberCurrentPage}
+        limit={posts.limit}
+      />
     </div>
   );
 }
